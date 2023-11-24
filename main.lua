35f1a610a30f138445cf5980eaa0867a3a6f24b6
GuglioIsStupid <bradsbluetab@gmail.com>
2023-11-24 15:54:31 -0500

    i hate this sm 💀

--- main.lua
+++ main.lua
@@ -32,6 +32,20 @@ func love*draw() {
 if (!love) {
     console.print.line("love is not defined")
 }
+
+// classes
+class Person {
+    func new(name) {
+        this.name = name
+    }
+
+    func sayHello() {
+        console.print.line("Hello, my name is " + this.name)
+    }
+}
+
+var p = Person("Bob")
+p:sayHello()
 ]]
 
 -- load main.stupid if it exists
@@ -47,36 +61,42 @@ end
 -- convert our code to lua code
 codeLines = {}
 for line in codeStr:gmatch("[^\r\n]+") do
-    print(line)
     table.insert(codeLines, line)
 end
 
 -- convert our code to lua code
 luaCodeLines = ""
+
+class = require("libs.class")
+local inFunction, inIf, inFor, inWhile, inClass = false, false, false, false, false
+local curClass = nil
 for i, line in ipairs(codeLines) do
     -- Comments
     line = line:gsub("//", "--")
     line = line:gsub("/%*", "--[[")
     line = line:gsub("%*/", "--]]")
 
-    -- Functions
+    -- Predefined functions
     line = line:gsub("console.print.line", "print")
     line = line:gsub("str", "tostring")
-    if line:find("LoadLuaCode") then
-        line = line .. "()"
-    end
     line = line:gsub("LoadLuaCode", "load")
 
     -- Defined functions
     -- replace * with . if func in line
     if line:find("func") then
-        line = line:gsub("%*", ".")
+        inFunction = true
     end
+
+    if inFunction then
+        --line = line:gsub("%*", ".")
+        line = line:gsub("{", " ")
+        if line:find("}") then
+            line = line:gsub("}", "\nend") --\n for safety
+            inFunction = false
+        end
+    end
+
     line = line:gsub("func", "function")
-    line = line:gsub("{", " ")
-    line = line:gsub("}", "end")
-    -- if before end theres a normal character, add a new line
-    line = line:gsub("end", "end\n")
 
     -- Variables
     line = line:gsub("var ", "")
@@ -86,33 +106,64 @@ for i, line in ipairs(codeLines) do
     if line:find("love*") then
         line = line:gsub("%*", ".")
     end
-    if line:find("*graphics*") then
-        line = line:gsub("%*", ".")
-    end
-    if line:find("*draw") then
-        line = line:gsub("%*", ".")
-    end
+
 
     -- Operators. only replace if not in a string
-    local quoteFound = false
-    for i = 1, #line do
-        local char = line:sub(i, i)
-        if char == "\"" then
-            quoteFound = not quoteFound
-        end
-        if char == "+" and not quoteFound then
-            line = line:sub(1, i - 1) .. ".." .. line:sub(i + 1)
-        end
-    end
+
 
     if line:find("if") then
+        inIf = true
+        -- replace { with then
+        line = line:gsub("{", "then")
+    end
+
+    if inIf then
         line = line:gsub("if", "if ")
         line = line:gsub("!", "not ")
         line = line:gsub("{", "then")
-        line = line:gsub("}", "end")
-        line = line:gsub("then", "then\n")
+        if line:find("}") then
+            line = line:gsub("}", "\nend") --\n for safety
+            inIf = false
+        end
 
-        line = line .. "then"
+        -- || and &&
+        line = line:gsub("||", "or")
+        line = line:gsub("&&", "and")
+    end
+
+    if line:find("class ") then
+        inClass = true
+    
+        -- get class name
+        curClass = line:match("class (.*)")
+        -- replace class with class:extend()
+        line = line:gsub("class " .. curClass, curClass .. " = class:extend()")
+    end
+
+    if inClass then
+        -- replace func with function
+        line = not line:find("function") and line:gsub("func", "function") or line
+        if line:find("function") then
+            --add curClass before function name
+            local funcName = line:match("function (.*)%(")
+            line = line:gsub("function " .. funcName, "function " .. curClass .. ":" .. funcName)
+        end
+
+        -- replace this with self
+        line = line:gsub("this", "self")
+
+        -- replace } with nothing, and set inClass to false
+        if line:find("}") then
+            line = line:gsub("}", "")
+            inClass = false
+        end
+    end
+
+    -- if any }, just replace with end
+    line = line:gsub("}", "end")
+
+    if line:find("if") and not line:find("then") then
+        line = line .. " then"
     end
 
     luaCodeLines = luaCodeLines .. line .. "\n"
